bd2a0929e918566bee69e96d6ced7b9890cfff39
safaroque7 <faroque.computer@gmail.com>
2024-02-27 17:55:45 +0600

    first commit

---
+++ assets/js/my-js.js
@@ -0,0 +1,15 @@
+$(window).load(function() {
+  $('.flexslider').flexslider({
+    animation: "slide",
+    animationLoop: true,
+    itemWidth: 210,
+    itemMargin: 50,
+    mousewheel: true,
+    rtl: true,
+    controlNav: false,
+    move: 1,
+    slideshow: 1,
+    animationSpeed: 350,
+    slideshowSpeed: 1000
+  });
+});

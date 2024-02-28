0393c7d4da7bf6bbd7b64336f1b2bd03ce866416
safaroque7 <faroque.computer@gmail.com>
2024-02-28 15:34:44 +0600

    our concern logo size small and speed discreaed

--- assets/js/my-js.js
+++ assets/js/my-js.js
@@ -2,14 +2,14 @@ $(window).load(function() {
   $('.flexslider').flexslider({
     animation: "slide",
     animationLoop: true,
-    itemWidth: 210,
-    itemMargin: 50,
+    itemWidth: 200,
+    itemMargin: 100,
     mousewheel: true,
     rtl: true,
     controlNav: false,
     move: 1,
     slideshow: 1,
-    animationSpeed: 350,
-    slideshowSpeed: 1000
+    animationSpeed: 500,
+    slideshowSpeed: 1500
   });
 });
